739185d0340c16c26c3e9cab2abf9b9d93c006a8
Marcos Giannini <marcogfa@gmail.com>
2025-07-05 14:53:24 +0000

    refactor: Eliminar AppLayout de las escenas de personajes

--- src/scenes/character-collection.scene.tsx
+++ src/scenes/character-collection.scene.tsx
@@ -1,12 +1,8 @@
 // src/scenes/character-collection.scene.tsx
 
 import * as React from 'react';
-// Usamos nuestro alias @ para que las rutas funcionen
-import { AppLayout } from '@/layouts';
 import { CharacterCollectionContainer } from '@/pods/character-collection';
 
 export const CharacterCollectionScene: React.FC = () => (
-  <AppLayout>
-    <CharacterCollectionContainer />
-  </AppLayout>
+  <CharacterCollectionContainer />
 );
--- src/scenes/character.scene.tsx
+++ src/scenes/character.scene.tsx
@@ -1,12 +1,9 @@
 // src/scenes/character.scene.tsx
 
 import * as React from 'react';
-// Corregimos los alias
-import { AppLayout } from '@/layouts';
 import { CharacterContainer } from '@/pods/character';
 
+// Aquí también quitamos el AppLayout.
 export const CharacterScene: React.FC = () => (
-  <AppLayout>
-    <CharacterContainer />
-  </AppLayout>
+  <CharacterContainer />
 );
